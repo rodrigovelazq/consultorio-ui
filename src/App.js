27d279e3d6320864dbf2bc1665e5f4c9e4a0e676
Rodrigo Velázquez <rodrigo.24.06.90@gmail.com>
2021-08-06 23:39:26 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './components/dashboard/Dashboard';
+
+function App() {
+  return (
+    <Dashboard></Dashboard>
+  );
+}
+
+export default App;
